cd1b04def00900b0cb5847228100a6b6488c0904
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-07-20 15:46:33 +0300

    added failed test

--- test/vars.spec.js
+++ test/vars.spec.js
@@ -2,4 +2,8 @@ const {fortyTwo} = require('../src/vars');
 
 test('fortyTwo() should return 42', () => {
     expect(fortyTwo()).toEqual(42);
-})
+});
+
+test('failed fortyTwo() should return 43', () => {
+    expect(fortyTwo()).toEqual(43);
+});
